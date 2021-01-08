cfca3def781bc9c94a88f06dec1a5452dbfb5481
tanay123456789 <tanaydwivedi456@gmail.com>
2021-01-08 13:47:13 +0530

    feat:Added main

--- main/app.js
+++ main/app.js
@@ -2,14 +2,25 @@ var btnTransalate=document.querySelector("#btn-transalate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 
+  var serverURL="https://api.funtranslations.com/translate/minion.json"
 
-//outputDiv.innerText="Tanay Pratap"
-console.log(outputDiv);
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server!try again after some time")
+}
 
 function clickHandler(){
-    outputDiv.innerText="ajssjsjsj"+txtInput.value;
+    var inputText=txtInput.value;
     
-    
+    fetch(getTranslationURL(inputText))
+    .then(response =>response.json())
+    .then(json=>console.log(json.contents.translated))
+    .catch(errorHandler)
+
 };
 
 
